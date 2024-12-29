fc5f280b735ade4dcaa7ea235e23dafda8e7cb48
ajlevy <AJLevy125@icloud.com>
2024-12-29 02:07:26 -0500

    5.5

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+
+
+
 async function getWeather(place){
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?key=SGHTYEA8MDGZ5XUUZHVMH2ZJV`, {mode: 'cors'})
     const weatherInfo = await response.json();
-    const location = weatherInfo.address
-    const datetime = weatherInfo.days[0].datetime
-    const highTemp = weatherInfo.days[0].tempmax
-    const lowTemp = weatherInfo.days[0].tempmin
-    const temp = weatherInfo.days[0].temp
-    const description = weatherInfo.days[0].description
-    
+    return {
+        location: weatherInfo.address,
+        datetime: weatherInfo.days[0].datetime,
+        highTemp: weatherInfo.days[0].tempmax,
+        lowTemp: weatherInfo.days[0].tempmin,
+        temp: weatherInfo.days[0].temp,
+        description: weatherInfo.days[0].description
+    };
 }
 
 
@@ -16,12 +20,15 @@ getWeather("Charlotte, NC")
 const weatherForm = document.getElementById("myForm")
 const weatherButton = document.getElementById("Black")
 const weatherInput = document.getElementById("Place")
-const weatherDiv = document.createElement("div")
 
-weatherButton.addEventListener("click", (e)=> {
+
+
+weatherButton.addEventListener("click", async (e)=> {
     e.preventDefault();
-    getWeather(weatherInput.value)
-})
+    const weatherData = await getWeather(weatherInput.value)
+    console.log("Weather Data:", weatherData)
+    
+});
 
 
 
